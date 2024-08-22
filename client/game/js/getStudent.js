3259061f702a9075075cfc0a15b200cc23869f95
RanMC-9918 <rohanjain0725@gmail.com>
2024-08-22 01:47:42 -0500

    first commit

---
+++ client/game/js/getStudent.js
@@ -0,0 +1,83 @@
+let studentNum = 0;
+const studentElem = document.getElementById("student");
+const studentImg = document.getElementById("studentImg");
+const indicator = document.getElementById("indicator");
+let answer;
+let url = "http://localhost:5500";
+loadNewStudent(studentNum);
+let selected;
+const choices = document.getElementsByClassName("answer-choices");
+
+for (let i = 0; i < choices.length; i++) {
+  choices[i].onclick = () => {
+    selected = i;
+  };
+}
+
+studentElem.onclick = () => {
+  if (selected == 0 && answer == "9") {
+    correct();
+  } else if (selected == 1 && answer == "10") {
+    correct();
+  } else if (selected == 2 && answer == "11") {
+    correct();
+  } else if (selected == 3 && answer == "12") {
+    correct();
+  } else {
+    incorrect();
+  }
+  studentNum++;
+  loadNewStudent(studentNum);
+};
+
+loading = "false";
+
+function loadNewStudent(id) {
+  fetch(url + "/api/" + id)
+    .then((response) => {
+      if (!response.ok || studentNum > 3) {
+        alert(
+          "You have exhausted our list of students 😅. \n Please add more students to this list: https://docs.google.com/spreadsheets/d/1VP6qbS5aN7p9ffNe3-yUo2puLxeFXA1rMKXKtIq8cX0/edit?usp=sharing"
+        );
+        throw new Error("Network response was not ok");
+      }
+      return response.text(); // Assuming the API returns JSON
+    })
+    .then((data) => {
+      return data; // Assuming the API returns
+    })
+    .then((data) => {
+      console.log("API call successful");
+      console.log(data); // Do something with the data
+      answer = data.split("\n")[1];
+      console.log(answer);
+      store(data.split("\n")[0]);
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
+
+function store(item) {
+  console.log("image changed");
+  studentImg.src = item;
+}
+
+function correct() {
+  indicator.style.zIndex = "100";
+  indicator.style.animation = "indicate 0.5s ease-in-out";
+  setTimeout(() => {
+    indicator.style.animation = "";
+    indicator.style.zIndex = "-1";
+  }, 1000);
+}
+
+function incorrect() {
+  indicator.style.zIndex = "100";
+  indicator.style.backgroundColor = "red";
+  indicator.style.animation = "indicate 0.3s ease-in-out";
+  setTimeout(() => {
+    indicator.style.animation = "";
+    indicator.style.zIndex = "-1";
+  }, 1000);
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { parse } = require("csv-parse");
+const fs = require("fs");
+let results = [];
+
+fs.createReadStream("./students.csv")
+  .pipe(parse({ delimiter: ",", from_line: 1 }))
+  .on("data", (row) => {
+    console.log(row);
+    results.push(row);
+  })
+  .on("error", (error) => {
+    console.error(error.message);
+  })
+  .on("end", () => {
+    console.log("Finished reading CSV file");
+  });
+
+const app = express();
+
+const port = process.env.PORT || 5500; // set port to either environment variable or 5500 if not set
+
+app.listen(port);
+console.log("listening on port " + port);
+
+app.use(express.static("client"));
+
+app.get("/api/:id", function (req, res) {
+  console.log("new api call");
+  let studentNum = req.params.id;
+  res.send("http:/images/" + studentNum + "\n" + results[studentNum][0]);
+});
+
+app.get("/images/:id", (req, res) => {
+  console.log("new image call: " + req.params.id);
+  res.sendFile(`images\\${req.params.id}.jpg`, { root: __dirname });
+});
+
+app.get("/game", (req, res) => {
+   express.static("client\\game");
+})
