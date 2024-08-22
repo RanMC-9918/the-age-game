6b5b0b662bc450759bee0b5b31aa43d825805164
Rohan <106928402+RanMC-9918@users.noreply.github.com>
2024-08-22 14:43:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -29,16 +29,14 @@ app.get("/api/:id", function (req, res) {
   console.log("new api call");
   let studentNum = req.params.id;
   res.send(
-    "https://the-age-game.onrender.com/images/" +
-      studentNum +
-      "\n" +
-      results[studentNum][0]
+    results[id][2 + "\n" +
+      results[studentNum][1]
   );
 });
 
 app.get("/images/:id", (req, res) => {
   console.log("new image call: " + req.params.id);
-  res.sendFile(`images/${req.params.id}.jpg`, { root: __dirname });
+  res.sendFile(`images/${req.params.id}.jpg`, { root: __dirname );
 });
 
 app.get("/game", (req, res) => {
